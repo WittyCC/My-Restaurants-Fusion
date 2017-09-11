160239d8a7a9e739649887acabfd72f109c5f311
Guest User <Guest@epicodus-8.local>
2017-09-11 11:31:22 -0700

    add url request

---
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -0,0 +1,15 @@
+package com.epicodus.myrestaurants;
+
+/**
+ * Created by Guest on 9/11/17.
+ */
+
+public class Constants {
+//    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+//    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_ACCESS_TYPE = BuildConfig.YELP_ACCESS_TYPE;
+    public static final String YELP_ACCESS_TOKEN = BuildConfig.YELP_ACCESS_TOKEN;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=food";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
+
